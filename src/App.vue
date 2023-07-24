aeb314b6cabcaaf37d6e4ce482a9c864fb1b67c7
Vitaliy Kovalev <vkovalev@synergy.ru>
2023-07-25 00:59:44 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div
+    .greet {{ message }}
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            message: 'Hello World'
+        }
+    }
+}
+</script>
+
+<style>
+.greet {
+    color: red;
+    font-weight: 500;
+    font-size: 20px;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+const path = require('path')
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.pug$/,
+                use: 'pug-plain-loader',  
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    'style-loader', 
+                    'css-loader', 
+                    'postcss-loader', 
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'templateRemoteEntry',
+            filename: 'templateRemoteEntry.entry.js',
+            remotes: {},
+            exposes: {},
+            shared: {}
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist']
+                }
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new VueLoaderPlugin()
+        ],
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        static: {
+          directory: path.join(__dirname),
+        },
+        compress: true,
+        port: 5050,
+        hot: true,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+        },
+    },
+  };
